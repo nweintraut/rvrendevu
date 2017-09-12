5a3cd599fa9735b64eb70d19da4f82e77978a192
Neil Weintraut <neil@Neils-iMac.local>
2017-09-12 12:54:56 -0700

    C

--- rxrendevu/RVLoginViewController.swift
+++ rxrendevu/RVLoginViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import IHKeyboardAvoiding
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class RVLoginViewController: RVBaseViewController {
     
@@ -15,15 +18,32 @@ class RVLoginViewController: RVBaseViewController {
     @IBOutlet weak var emailMessageLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var emailView: UIView!
     @IBOutlet weak var passwordMessageLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         KeyboardAvoiding.avoidingView = loginView
-        emailTextField.delegate = self
         passwordTextField.delegate = self
+        //emailTextField.delegate = self
+        emailTextField.rx.setDelegate(self).disposed(by: rx_disposeBag)
+        emailTextField.rx.textFieldDidBeginEditing.subscribe(onNext: { (alwaysTrue) in
+            print("In textFieldDidBeginEditing)")
+        }).disposed(by: rx_disposeBag)
+
+        emailTextField.rx.textFieldDidEndEditing.subscribe(onNext: { (alwaysTrue) in
+            print("In textFieldDidEndEditing)")
+            }).disposed(by: rx_disposeBag)
+        
+        emailTextField.rx.textFieldDidEndEditingWithReason.subscribe(onNext: { (reason) in
+            print("In textFieldDidEndEditingWithReason: \(reason.rawValue))")
+        }).disposed(by: rx_disposeBag)
+        
     }
+
 }
 
 extension RVLoginViewController: UITextFieldDelegate {
@@ -43,7 +63,9 @@ extension RVLoginViewController: UITextFieldDelegate {
     }
     
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
         if textField == self.emailTextField {
+            print("In \(self.classForCoder).making passwordField first responders")
             self.passwordTextField.becomeFirstResponder()
         }
 
@@ -52,5 +74,106 @@ extension RVLoginViewController: UITextFieldDelegate {
         }
         return true
     }
+    func loginFailure(hide: Bool) {
+        
+    }
+    func showHideEmailMessage(message: String, show: Bool) {
+        
+    }
+    func showHidePasswordMessage(message: String, show: Bool) {
+        
+    }
+    func hideButtons() {
+    
+    }
+    func hideView(view: UIView?) {
+        if let view = view { view.isHidden = true }
+    }
+    func showView(view: UIView?) {
+        if let view = view { view.isHidden = false }
+    }
+    
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("In \(self.classForCoder).shouldChange, textFieldText: \(textField.text!) and newString is:\(string) \(string.characters.count)")
+        self.loginFailure(hide: true)
+        let emailField = (self.emailTextField != nil) && ( textField == self.emailTextField)
+        let passwordField = (self.passwordTextField != nil) && (textField == self.passwordTextField)
+        let text = textField.text ?? ""
+        let count = text.characters.count
+        let combined = "\(text)\(string)"
+        if string == "" {
+            // nothing new
+            if count <= 1 {
+                if emailField {
+                    showHideEmailMessage(message: "Invalid Email", show: true)
+                    hideButtons()
+                    return true
+                } else if passwordField {
+                    showHidePasswordMessage(message: "Invalid Password", show: true)
+                    hideButtons()
+                } else {
+                    return false
+                }
+            } else {
+                let candidate = text.substring(to: count - 1)
+                if emailField {
+                    if let message = candidate.isValidEmail() {
+                        showHideEmailMessage(message: message, show: true)
+                        hideView(view: passwordView)
+                        hideButtons()
+                    } else {
+                        
+                    }
+                }
+            }
+        } else {
+            
+        }
+        return true
+    }
+ 
     
 }
+
+
+/*
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ KeyboardAvoiding.avoidingView = loginView
+ //  emailTextField.delegate = self
+ 
+ /*      let goober = emailTextField.rx.controlEvent(UIControlEvents.allEvents).asObservable()
+ .map { stuff in
+ print("In map \(stuff)")
+ return self.emailTextField.text!
+ }
+ .flatMap { text in
+ return Observable.just(text)
+ }
+ .asDriver(onErrorJustReturn: "Error")
+ goober.map{ goofy in  return self.emailTextField.text! }
+ .drive(passwordTextField.rx.text)
+ */
+ 
+ passwordTextField.delegate = self
+ emailTextField.delegate = self
+ emailTextField.rx.text
+ .filter {
+ print("g \(String(describing: $0))")
+ return ($0 ?? "").characters.count > 0
+ }
+ 
+ .flatMapLatest({ (text: String?) -> Observable<Any> in
+ 
+ return Observable.empty()
+ })
+ .shareReplay(1)
+ .observeOn(MainScheduler.instance)
+ .subscribe(onNext: { data in
+ 
+ }).disposed(by: rx_disposeBag)
+ 
+ }
+ */
+
+
---
+++ rxrendevu/RVLoginViewKeyboardAvoidingTextFieldProxy.swift
@@ -0,0 +1,95 @@
+//
+//  RVLoginViewKeyboardAvoidingTextFieldProxy.swift
+//  rxrendevu
+//
+//  Created by Neil Weintraut on 9/12/17.
+//  Copyright © 2017 Neil Weintraut. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class RVTextFieldProxy: DelegateProxy {
+
+
+}
+
+extension RVTextFieldProxy: DelegateProxyType {
+    class func setCurrentDelegate(_ delegate: AnyObject?, toObject object: AnyObject) {
+        if let textField = object as? UITextField {
+            if let delegate = delegate as? UITextFieldDelegate {
+                textField.delegate = delegate
+            } else {
+                print("In \(self.classForCoder()).setCurrent Delegate, delegate \(delegate ?? "no-delegate" as AnyObject) is not a UITextFieldDelegate")
+            }
+        } else {
+            print("In \(self.classForCoder()).setCurrent Delegate, Object \(object) is nota UITextField")
+        }
+    }
+    class func currentDelegateFor(_ object: AnyObject) -> AnyObject? {
+        if let textView = object as? UITextField {
+            return textView.delegate
+        } else {
+            print("In \(self.classForCoder()).currentDelegate, Object \(object) is not a UITextField")
+            return nil
+        }
+    }
+    
+}
+
+// Page 259 in RxSwift Book
+extension Reactive where Base: UITextField {
+    var delegate: DelegateProxy {
+        return RVTextFieldProxy.proxyForObject(base)
+    }
+    public func setDelegate(_ delegate: UITextFieldDelegate) -> Disposable {
+        return RVTextFieldProxy.installForwardDelegate(
+            delegate,
+            retainDelegate: false,
+            onProxyForObject: self.base
+        )
+    }
+
+    public var textFieldDidBeginEditing: ControlEvent<Bool> {
+        let source = delegate
+            .methodInvoked(#selector(UITextFieldDelegate.textFieldDidBeginEditing(_:)))
+        .map { parameters -> Bool in
+            return true
+        }
+        return ControlEvent(events: source)
+    }
+
+    public var textFieldDidEndEditing: ControlEvent<Bool> {
+        let source = delegate
+            .methodInvoked(#selector(UITextFieldDelegate.textFieldDidEndEditing(_:)))
+            .map { parameters -> Bool in
+                return true
+        }
+        return ControlEvent(events: source)
+    }
+    public var textFieldDidEndEditingWithReason: ControlEvent<UITextFieldDidEndEditingReason> {
+        let source = delegate
+            .methodInvoked(#selector(UITextFieldDelegate.textFieldDidEndEditing(_:reason:)))
+            .map { parameters -> UITextFieldDidEndEditingReason in
+                (parameters[1] as? UITextFieldDidEndEditingReason) ?? UITextFieldDidEndEditingReason.committed
+        }
+        return ControlEvent(events: source)
+    }
+}
+
+extension RVTextFieldProxy: UITextFieldDelegate {
+
+}
+/*
+ func textFieldDidBeginEditing(_ textField: UITextField) // became first responder
+ func textFieldDidEndEditing(_ textField: UITextField) // may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
+  func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) // if implemented, called in place of textFieldDidEndEditing:
+ 
+ func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool // return NO to disallow editing.
+ func textFieldShouldEndEditing(_ textField: UITextField) -> Bool // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+ func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool // return NO to not change text
+ func textFieldShouldClear(_ textField: UITextField) -> Bool // called when clear button pressed. return NO to ignore (no notifications)
+ func textFieldShouldReturn(_ textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+ 
+ */
