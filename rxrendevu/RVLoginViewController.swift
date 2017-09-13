eab5e2d009e75c9bc1d8f49bffe7ec6c8e366ebd
Neil Weintraut <neil@Neils-iMac.local>
2017-09-13 16:43:06 -0700

    c

--- rxrendevu/RVLoginViewController.swift
+++ rxrendevu/RVLoginViewController.swift
@@ -14,9 +14,13 @@ import NSObject_Rx
 
 class RVLoginViewController: RVBaseViewController {
     
+    @IBOutlet weak var goofyView: UIView!
     @IBOutlet weak var loginView: UIView!
     @IBOutlet weak var emailMessageLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
+
+    @IBOutlet weak var loginButtonView: UIView!
+    @IBOutlet weak var registerButtonView: UIView!
     
     @IBOutlet weak var passwordView: UIView!
     @IBOutlet weak var emailView: UIView!
@@ -28,40 +32,45 @@ class RVLoginViewController: RVBaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.goofyView.layer.borderWidth = 5.0
+        self.goofyView.layer.borderColor = UIColor.facebookBlue().cgColor
         self.hideView(view: self.emailMessageLabel)
+        self.hideView(view: self.passwordView)
+        self.hideView(view: self.passwordMessageLabel)
+        self.hideLoginRegisterButtonViews()
         KeyboardAvoiding.avoidingView = loginView
         emailTextField.rx.setDelegate(self).disposed(by: rx_disposeBag)
+     //   passwordTextField.delegate = self
         passwordTextField.rx.setDelegate(self).disposed(by: rx_disposeBag)
         
         emailTextField.rx.textFieldDidBeginEditing.subscribe(onNext: { (alwaysTrue) in
-            print("In textFieldDidBeginEditing)")
+            print("In email rx.textFieldDidBeginEditing")
         }).disposed(by: rx_disposeBag)
 
         emailTextField.rx.textFieldDidEndEditing.subscribe(onNext: { (alwaysTrue) in
-            print("In textFieldDidEndEditing)")
+            print("In email rx.textFieldDidEndEditing)")
             }).disposed(by: rx_disposeBag)
         
+        passwordTextField.rx.textFieldDidBeginEditing.subscribe(onNext: { (alwaysTrue) in
+            print("In password rx.textFieldDidBeginEditing")
+        }).disposed(by: rx_disposeBag)
+        
+        passwordTextField.rx.textFieldDidEndEditing.subscribe(onNext: { (alwaysTrue) in
+            print("In password rx.textFieldDidEndEditing)")
+        }).disposed(by: rx_disposeBag)
+        /*
         emailTextField.rx.textFieldDidEndEditingWithReason.subscribe(onNext: { (reason) in
             print("In textFieldDidEndEditingWithReason: \(reason.rawValue))")
         }).disposed(by: rx_disposeBag)
-        
-        let emailInput = emailTextField.rx.text
-            .filter { (input: String?) -> Bool in
-          //      print("In \(self.classForCoder)emailTextField.rx.text: \(String(describing: input))")
-                return (input ?? "").characters.count > 0
-            }
-            .flatMap({ (text: String?) -> Observable<String> in
-                return Observable<String>.just(text!)
-            })
-        emailInput  .subscribe(onNext: { (e: String) in
-            print("In \(self.classForCoder).emailInput.subscribe: \(e)")
-        }).disposed(by: rx_disposeBag)
+        */
+
         
         let emailLookup = emailTextField.rx.text
             .filter {(input: String?) -> Bool in
                 self.hideView(view: self.passwordView)
                 if let candidate = input {
                     if let message = candidate.isValidEmail() {
+                        self.hideLoginRegisterButtonViews()
                         self.showView(view: self.emailMessageLabel)
                         self.emailMessageLabel.text = message
                         return false
@@ -74,6 +83,7 @@ class RVLoginViewController: RVBaseViewController {
                 else { return false }
             }
             .map { (input: String?) -> String in return (input != nil) ? input! : "" }
+            .distinctUntilChanged()
             .throttle(3.0, scheduler: MainScheduler.instance)
             .flatMap { (email) -> Observable<String> in
             return RVMeteorService.sharedInstance.rx.findAccountViaEmail(email: email.lowercased())
@@ -84,42 +94,115 @@ class RVLoginViewController: RVBaseViewController {
         emailLookup.subscribe(onNext: { (email) in
             print("In emailLookup with: \(email)")
             if email != "" {
-               
+               self.showLoginRegisterButtonView(login: true)
             } else {
-                
+               self.showLoginRegisterButtonView(login: false)
             }
         }, onError: { (error) in
             print(error)
         }, onCompleted: {
             print("In emailLookup on Completed")
         }).disposed(by: rx_disposeBag)
+        
+        
+        /*
+        let passwordLookup = passwordTextField.rx.text
+            .filter {(input: String?) -> Bool in
+               // self.hideView(view: self.passwordView)
+                if let candidate = input {
+                    if let message = candidate.isValidPassword() {
+                  //      self.hideLoginRegisterButtonViews()
+                  //      self.showView(view: self.emailMessageLabel)
+                 //       self.emailMessageLabel.text = message
+                        return false
+                    } else {
+                  //      self.hideView(view: self.emailMessageLabel)
+                  //      self.showView(view: self.passwordView)
+                        return true
+                    }
+                }
+                else { return false }
+            }
+            .map { (input: String?) -> String in return (input != nil) ? input! : "" }
+            .throttle(3.0, scheduler: MainScheduler.instance)
+            .flatMap { (email) -> Observable<String> in
+                return Observable<String>.just("Password")
+            }
+            .shareReplay(1)
+            .observeOn(MainScheduler.instance)
+        passwordLookup.subscribe(onNext: { (password) in
+            print("In passwordLookup with: \(password)")
+            if password != "" {
+               // self.showLoginRegisterButtonView(login: true)
+            } else {
+              //  self.showLoginRegisterButtonView(login: false)
+            }
+        }, onError: { (error) in
+            print(error)
+        }, onCompleted: {
+            print("In passwordLookup on Completed")
+        }).disposed(by: rx_disposeBag)
+        
+        */
     }
     
+    func showLoginRegisterButtonView(login: Bool) {
+        if let loginView = self.loginButtonView {
+            loginView.isHidden = !login
+        }
+        if let registerView = self.registerButtonView {
+            registerView.isHidden = login
+        }
+    }
+    func hideLoginRegisterButtonViews() {
+        self.hideView(view: self.loginButtonView)
+        self.hideView(view: self.registerButtonView)
+    }
+    func emailOrPassword(textField: UITextField) -> String {
+  
+        if (textField == self.emailTextField) { return "Email" }
+        return "Password"
+    }
 
 }
 
 extension RVLoginViewController: UITextFieldDelegate {
     // Optional
     // These delegate methods can be used so that test fields that are hidden by the keyboard are shown when they are focused
-    public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("IN \(self.classForCoder).textFieldShouldBeginEditing \(self.emailOrPassword(textField: textField))")
         if textField == self.emailTextField {
             KeyboardAvoiding.padding = 20
-            KeyboardAvoiding.avoidingView = textField
+            KeyboardAvoiding.avoidingView = self.goofyView
+            //KeyboardAvoiding.padding = 300
         }
 
         else if textField == self.passwordTextField {
-            KeyboardAvoiding.padding = 20
-            KeyboardAvoiding.avoidingView = self.loginView
-            KeyboardAvoiding.padding = 0
+                        KeyboardAvoiding.padding = 200
+            KeyboardAvoiding.avoidingView = self.passwordView
+          //  self.showView(view: self.passwordView)
+
+      //      KeyboardAvoiding.avoidingView = textField
+          //  KeyboardAvoiding.padding = 0
         }
         return true
     }
     
-    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("In \(self.classForCoder).textFieldShouldEndEditing \(self.emailOrPassword(textField: textField))")
+     //   if textField == self.emailTextField { self.passwordTextField.becomeFirstResponder() }
+     //   else if textField == self.passwordTextField { textField.resignFirstResponder() }
+//        if textField == self.passwordTextField { return false }
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("In \(self.classForCoder).textFieldShouldReturn \(self.emailOrPassword(textField: textField))")
         if textField == self.emailTextField { self.passwordTextField.becomeFirstResponder() }
         else if textField == self.passwordTextField { textField.resignFirstResponder() }
         return true
     }
+    
     func loginFailure(hide: Bool) {
         
     }
@@ -135,6 +218,7 @@ extension RVLoginViewController: UITextFieldDelegate {
 
     
     public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("In \(self.classForCoder).shouldChangeCharacters. String: [\(string)]")
         if (self.emailTextField != nil) && (self.emailTextField == textField) {
             return emailTextEvaluator.evaluate(text: textField.text, replacementRange: range, replacementString: string)
         } else if (self.passwordTextField != nil) && (self.passwordTextField == textField) {
